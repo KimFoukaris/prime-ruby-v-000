2a6d0a2fa0493c6216151c67a4eb84fd2b313c25
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-29 00:33:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,20 @@
 def prime?(integer)
-range = (2..integer).to_a
-range.each {|num| puts num}
+  range = (nil, nil, *2..integer).to_a
+  divisors = (2..Math.sqrt(integer)).to_a
+
+  range.each {|num| print "#{num}, "}
+  divisors.each {|num| print "D#{num}, "}
+
+  divisors.each do |num|
+    (num**2..integer).step(1){|place| range[place] = nil} if range[num]
+  end
+  range.compact
+
+  ##prime.each {|num| print "P#{num} "}
+  ##not_prime.each {|num| print "NP#{num} "}
+  if range.include?(integer) == true
+    true
+  else
+    false
+  end
 end
