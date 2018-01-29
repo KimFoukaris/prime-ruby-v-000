3f7119bc293778c161e12276871483e5d21ef706
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-29 00:53:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,24 @@
 def prime?(integer)
+
+  # negative numbers not prime
   if integer < 0
     return false
   end
-  range = [nil, nil, *2..integer]
 
-  range.each {|num| print "#{num}, "}
+  # use Eratosthense Sieve
+  range = [nil, nil, *2..integer]  #create range
 
-  (2..Math.sqrt(integer)).each do |num|
-    (num**2..integer).step(num){|place| range[place] = nil} if range[num]
+  # print to test
+  # range.each {|num| print "#{num}, "}
+
+  (2..Math.sqrt(integer)).each do |num|  # for each divisor
+    (num**2..integer).step(num){|place| range[place] = nil} if range[num]  # replace non-prime numbers with nil
   end
-  range.compact
-  range.each {|num| print "#{num}, "}
+  range.compact  # remove nil values from range
+
+  #print to test
+  #range.each {|num| print "#{num}, "}
+  
   if range.include?(integer) == true
     true
   else
